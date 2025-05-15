d43894a0259c23e2d4cf7911eef5f7c6aa6e430d
solomon-ctg <solomon.iroegbu@gmail.com>
2025-05-15 16:34:05 +0100

    initial

---
+++ food/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutterr',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Login')),
+        body: Container(
+          color: Theme.of(context).primaryColor,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Login button pressed');
+                },
+                child: Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 36),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
