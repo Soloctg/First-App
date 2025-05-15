a8338e42605bfe7851e19aed29ac84e14a293a28
solomon-ctg <solomon.iroegbu@gmail.com>
2025-05-15 18:56:23 +0100

    Styling Flutter Forms

--- food/lib/main.dart
+++ food/lib/main.dart
@@ -13,33 +13,48 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutterr',
       home: Scaffold(
         appBar: AppBar(title: Text('Login')),
+
         body: Container(
           color: Theme.of(context).primaryColor,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                ),
-              ),
-              TextField(
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('Login button pressed');
-                },
-                child: Text('Login'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  minimumSize: Size(double.infinity, 36),
+              Card(
+                elevation: 0,
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                      ),
+                      SizedBox(height: 20), // Acts as a spacer
+                      TextField(
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      ),
+                      SizedBox(height: 20), // Acts as a spacer
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Login button pressed');
+                        },
+                        child: Text('Login'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 40),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
